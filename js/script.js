968b2e5bb12851bd666c706020c424dde6f1f24d
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-28 16:48:17 +0200

    creata struttura base con chiamata API

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
+
+createApp({
+    data() {
+      return {
+        
+      };
+    },
+    created() {
+      
+    },
+    methods: {
+        getEmails: function() {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        }
+    }
+        
+  }).mount("#app");
